82a6604ba83228034806ac85096f356f205b66f5
Nikitin-Alexandr <79432079+Nikitin-Alexandr@users.noreply.github.com>
2021-02-22 09:55:05 +0700

    clear.sh

---
+++ clear.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+LOG=~/ex.log
+export LOG
+date >>$LOG
+# restart PostgreSQL
+systemctl stop postgresql-13
+#ps -ef|grep bin/postgres|grep -v grep
+#echo 'Restarting PostgreSQL'
+systemctl start postgresql-13
+#status of PostgreSQL
+#ps –ef|grep bin/postgres|grep –v grep
+free –m>>$LOG
+sync; echo 3 > /proc/sys/vm/drop_caches
+free –m>>$LOG
+rm -rf /var/lib/pgsql/test/*
+echo ‘================================================‘>>$LOG
